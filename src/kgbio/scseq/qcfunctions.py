87d3f61e8d1d34b92ea4fcfe4cc52663ecba6dbb
Kenny <33559196+gouinK@users.noreply.github.com>
2023-02-02 09:32:17 -0800

    bug fix

--- src/kgbio/scseq/qcfunctions.py
+++ src/kgbio/scseq/qcfunctions.py
@@ -200,7 +200,7 @@ def qc_filtering(adata=None, percentile=5, mtThresh=10, scrublet=True, do_plot=F
 	if scrublet:
 		doublet_rate = 0.07
 		sc.external.pp.scrublet(adata, expected_doublet_rate=doublet_rate)
-		adata = adata[~adata.obs['predicted_doublet']].copy()
+		adata = adata[~adata.obs['predicted_doublets']].copy()
 
 	geneThresh = np.percentile(adata.obs.n_genes_by_counts, q=percentile)
 	umiThresh = np.percentile(adata.obs.total_counts, q=percentile)
